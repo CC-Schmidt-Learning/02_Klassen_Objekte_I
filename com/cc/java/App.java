b821c7c47c098d8f10daf09678274e6e222628c8
Daniela <Daniela-Haeussler@gmx.de>
2022-11-22 09:16:27 +0100

    Int

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,11 +3,11 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+        output(2);
     }
 
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
+    public static void output(int i) {
+        System.out.println(i);
     }
 
 
