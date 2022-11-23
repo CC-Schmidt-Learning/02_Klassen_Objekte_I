e2c7fe016e6c9fdffd88c489db06eff7cace7af8
Daniela <Daniela-Haeussler@gmx.de>
2022-11-23 11:56:30 +0100

    Gets and Sets

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,30 +3,24 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-        Cat cat=new Cat();
-cat.name="Grisabella";
-cat.furColor="white";
-cat.age=29;
+        Cat cat=new Cat("Grizabella","white",29 );
 
 // output(cat.name);
 // output(cat.furColor);
 // output(String.valueOf(cat.age));
 
-output(cat.tellYourName());
-output(cat.tellYourfurColor());
-output(String.valueOf(cat.tellYourAge()));
+output(cat.getName());
+output(cat.getFurColor());
+output(String.valueOf(cat.getAge()));
 
 
  output("______________________");
 
-Cat cat1 = new Cat();
-cat1.name="Alonzo";
-cat1.furColor="grey";
-cat1.age=35;
-
-output(cat1.tellYourName());
-output(cat1.tellYourfurColor());
-output(String.valueOf(cat1.tellYourAge()));
+Cat cat1 = new Cat("Alonzo", "grey", 35);
+cat1.setFurColor("schwarz");
+output(cat1.getName());
+output(cat1.getFurColor());
+output(String.valueOf(cat1.getAge()));
     }
 
 
@@ -34,6 +28,5 @@ output(String.valueOf(cat1.tellYourAge()));
     public static void output(String name) {
         System.out.println(name);
     }
-
 }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,25 +2,47 @@ package com.cc.java;
 
 public class Cat {
 
-public static final String furcolor = null;
-public String name = "nobody";
-public String furColor;
-public int age;
+private String name;
+private String furColor;
+private int age;
+
+
+public void setName(String name) {
+  this.name = name;
+}
+
+
+public void setFurColor(String furColor) {
+  this.furColor = furColor;
+}
+
+
+public void setAge(int age) {
+  this.age = age;
+}
+
 
 // Ein Constructor ist immer ohne return-Wert 
-public Cat(String string, String string2, int i) {
-  
+public Cat(String name, String furColor, int age) {
+  this.name =name;
+  this.furColor=furColor;
+  this.age=age;
 }
 
 
-public String tellYourName() {
-return this.name;
+public String getName() {
+  return name;
 }
-public String tellYourfurColor() {
-  return this.furColor;
-  }
-public int tellYourAge() {
-    return this.age;
-    }
+
+
+public String getFurColor() {
+  return furColor;
+}
+
+public int getAge() {
+  return age;
+}
 
 }
+
+
